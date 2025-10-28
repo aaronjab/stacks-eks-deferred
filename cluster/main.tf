6bbf5287cd8c869d5aaf590f681d4b609c297fd9
“aaronjab” <aaron@jab.ski>
2025-10-28 13:43:44 -0400

    Free tier instance type for EKS node group

--- cluster/main.tf
+++ cluster/main.tf
@@ -52,6 +52,7 @@ resource "aws_eks_node_group" "demo" {
     max_size     = local.azCount
     min_size     = 1
   }
+  instance_types = ["t3.micro"]  # Free tier eligible instance
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
